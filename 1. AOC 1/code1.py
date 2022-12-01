8ab86ec762cbd97919fc7cd01403284b6dc3763f
s1117668 <s1117668@student.hsleiden.nl>
2022-12-01 10:05:20 +0100

    Dag 1 voltooid

--- 1. AOC 1/code1.py
+++ 1. AOC 1/code1.py
@@ -1,7 +1,19 @@
 def code():
+    elven = [] # alle elven die het eten dragen
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        inhoud = bestand.read().rstrip().split('\n')
+        calo_groep = []
+        for voedsel in inhoud:
+            # print(voedsel)
+            if voedsel == '':
+                elven.append(calo_groep)
+                calo_groep = []
+                continue
+            calo_groep.append(int(voedsel))
+    max_calo = []
+    for bundel in elven:
+        max_calo.append(sum(bundel))
+    return max(max_calo)
 
 
 if __name__ == '__main__':
--- 1. AOC 1/code2.py
+++ 1. AOC 1/code2.py
@@ -1,7 +1,24 @@
 def code():
+    elven = [] # alle elven die het eten dragen
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        inhoud = bestand.read().rstrip().split('\n')
+        calo_groep = []
+        for voedsel in inhoud:
+            # print(voedsel)
+            if voedsel == '':
+                elven.append(calo_groep)
+                calo_groep = []
+                continue
+            calo_groep.append(int(voedsel))
+    max_calo = []
+    top3 = 0
+    for bundel in elven:
+        max_calo.append(sum(bundel))
+    for i in range(3):
+        hoogste = max(max_calo)
+        top3 += hoogste
+        max_calo.remove(hoogste)
+    return top3
 
 
 if __name__ == '__main__':
