1eeb1979b5bc9f1435ce37c0587db2dbef7e4c0f
s1117668 <s1117668@student.hsleiden.nl>
2022-12-02 11:30:12 +0100

    Dag 2 optimalisatie (2)

--- 02. AOC Act II/code1.py
+++ 02. AOC Act II/code1.py
@@ -1,20 +1,19 @@
 def code():
-    eind_res = {('A', 'X'): 3,
-                ('B', 'Y'): 3,
-                ('C', 'Z'): 3,
-                ('A', 'Y'): 6,
-                ('B', 'Z'): 6,
-                ('C', 'X'): 6,
-                ('A', 'Z'): 0,
-                ('B', 'X'): 0,
-                ('C', 'Y'): 0}
+    eind_res = {('A', 'X'): 3 + 1,
+                ('B', 'Y'): 3 + 2,
+                ('C', 'Z'): 3 + 3,
+                ('A', 'Y'): 6 + 2,
+                ('B', 'Z'): 6 + 3,
+                ('C', 'X'): 6 + 1,
+                ('A', 'Z'): 0 + 3,
+                ('B', 'X'): 0 + 1,
+                ('C', 'Y'): 0 + 2}
     win = {'X': 1,
            'Y': 2,
            'Z': 3}
     with open('input.txt', 'r') as bestand:
         sps_tour = bestand.read().rstrip('\n').split('\n')
-    return sum([eind_res[tuple(x.split())] + \
-                win[x[-1]] for x in sps_tour])
+    return sum([eind_res[tuple(x.split())] for x in sps_tour])
 
 
 if __name__ == '__main__':
