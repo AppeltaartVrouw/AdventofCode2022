5ee7c91453048635d4e6c6fcb409342ed4348f85
s1117668 <s1117668@student.hsleiden.nl>
2022-12-02 10:38:36 +0100

    Dag 2 voltooid

--- 02. AOC Act II/code1.py
+++ 02. AOC Act II/code1.py
@@ -1,7 +1,24 @@
 def code():
+    eind_res = {('A', 'X'): 3,
+                ('B', 'Y'): 3,
+                ('C', 'Z'): 3,
+                ('A', 'Y'): 6,
+                ('B', 'Z'): 6,
+                ('C', 'X'): 6,
+                ('A', 'Z'): 0,
+                ('B', 'X'): 0,
+                ('C', 'Y'): 0}
+    win = {'X': 1,
+           'Y': 2,
+           'Z': 3}
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        sps_tour = bestand.read().rstrip('\n').split('\n')
+    tal = 0
+    for sps in sps_tour:
+        sps = tuple(sps.split())
+        print(sps)
+        tal += eind_res.get(sps) + win.get(sps[-1])
+    return tal
 
 
 if __name__ == '__main__':
--- 02. AOC Act II/code2.py
+++ 02. AOC Act II/code2.py
@@ -1,7 +1,20 @@
 def code():
+    eind_res = {('A', 'X'): 0+3,
+                ('C', 'X'): 0+2,
+                ('B', 'X'): 0+1,
+                ('B', 'Y'): 3+2,
+                ('A', 'Y'): 3+1,
+                ('C', 'Y'): 3+3,
+                ('C', 'Z'): 6+1,
+                ('B', 'Z'): 6+3,
+                ('A', 'Z'): 6+2,}
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        sps_tour = bestand.read().rstrip('\n').split('\n')
+    tal = 0
+    for sps in sps_tour:
+        sps_tup = tuple(sps.split())
+        tal += eind_res.get(sps_tup)
+    return tal
 
 
 if __name__ == '__main__':
