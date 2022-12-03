874ba16363dbd59ac4eb24b24c2a66ae2e7829e5
s1117668 <s1117668@student.hsleiden.nl>
2022-12-03 08:33:47 +0100

    Dag 3 voltooid

--- 03. AOC Legacy/code1.py
+++ 03. AOC Legacy/code1.py
@@ -1,7 +1,19 @@
+def tellen(regel):
+    punt = int(len(regel)/2)
+    h1 = set(regel[:punt])
+    h2 = set(regel[punt:])
+    letter = h1.intersection(h2)
+    return letter.pop()
+
+
 def code():
+    prio = {chr(x): j for j, x in enumerate(range(97, 123), start=1)}
+    prio.update((chr(x), j) for j, x in enumerate(range(65, 91), start=27))
+    prio_tal = 0
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        for regel in bestand:
+            prio_tal += prio[tellen(regel.rstrip())]
+    return prio_tal
 
 
 if __name__ == '__main__':
--- 03. AOC Legacy/code2.py
+++ 03. AOC Legacy/code2.py
@@ -1,7 +1,20 @@
+def tellen(h1, h2, h3):
+    h1, h2, h3 = set(h1), set(h2), set(h3)
+    h12 = h1.intersection(h2)
+    h23 = h2.intersection(h3)
+    h123 = h12.intersection(h23)
+    return h123.pop()
+
+
 def code():
+    prio = {chr(x): j for j, x in enumerate(range(97, 123), start=1)}
+    prio.update((chr(x), j) for j, x in enumerate(range(65, 91), start=27))
+    prio_tal = 0
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        inp = bestand.read().rstrip('\n').split('\n')
+    for reg in range(2, len(inp), 3):
+        prio_tal += prio[tellen(inp[reg-2], inp[reg-1], inp[reg])]
+    return prio_tal
 
 
 if __name__ == '__main__':
