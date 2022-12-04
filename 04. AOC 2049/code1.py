4cd960d1dc9aae9eb328bddd0ded8907b1029ce6
s1117668 <s1117668@student.hsleiden.nl>
2022-12-04 09:44:32 +0100

    Dag 4 voltooid

--- 04. AOC 2049/code1.py
+++ 04. AOC 2049/code1.py
@@ -1,7 +1,16 @@
 def code():
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        inhoud = bestand.read().rstrip().split('\n')
+    tal = 0
+    for idn in inhoud:
+        r1, r2 = idn.split(',')
+        r1, r2 = r1.split('-'), r2.split('-')
+        if (int(r1[0]) in range(int(r2[0]), int(r2[1])+1) and
+                int(r1[1]) in range(int(r2[0]), int(r2[1])+1)) or \
+            (int(r2[0]) in range(int(r1[0]), int(r1[1])+1) and
+             int(r2[1]) in range(int(r1[0]), int(r1[1])+1)):
+            tal += 1
+    return tal
 
 
 if __name__ == '__main__':
--- 04. AOC 2049/code2.py
+++ 04. AOC 2049/code2.py
@@ -1,7 +1,16 @@
 def code():
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        inhoud = bestand.read().rstrip().split('\n')
+    tal = 0
+    for idn in inhoud:
+        r1, r2 = idn.split(',')
+        r1, r2 = r1.split('-'), r2.split('-')
+        s1 = set(range(int(r1[0]), int(r1[1])+1))
+        s2 = set(range(int(r2[0]), int(r2[1]) + 1))
+        overlap = s1 & s2
+        if len(overlap) > 0:
+            tal += 1
+    return tal
 
 
 if __name__ == '__main__':
