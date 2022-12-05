5142aa7ee7d11b89a4a3f722228a00045b1ec8fb
s1117668 <s1117668@student.hsleiden.nl>
2022-12-05 18:50:37 +0100

    Dag 5 voltooid

--- 05. AOC 5 Diddy's Kong Quest/code1.py
+++ 05. AOC 5 Diddy's Kong Quest/code1.py
@@ -1,7 +1,29 @@
-def code():
+def parsen():
     with open('input.txt', 'r') as bestand:
-        pass
-    return 0
+        inhoud = bestand.read()
+    kratten = {}
+    kratten_index, bew_instr = inhoud.split('\n\n')
+    kol_num = int(kratten_index.split('\n')[-1][-1])
+    for i in range(1, kol_num+1):
+        kratten[i] = []
+    for rij in kratten_index.split('\n')[::-1][1:]:
+        for kolom, krat in enumerate(range(-1, len(rij)+1, 4)):
+            if krat < 0:
+                continue
+            letter = rij[krat-3:krat][1]
+            if letter != ' ':
+                kratten[kolom].append(rij[krat-3:krat][1])
+    return kratten, bew_instr.rstrip()
+
+
+def code():
+    kratten, bew_instr = parsen()
+    for instructie in bew_instr.split('\n'):
+        inst = instructie.split()
+        verp = kratten[int(inst[3])][-int(inst[1]):][::-1]
+        del kratten[int(inst[3])][-int(inst[1]):]
+        kratten[int(inst[5])].extend(verp)
+    return "".join(kratten[x][-1] for x in kratten.keys())
 
 
 if __name__ == '__main__':
